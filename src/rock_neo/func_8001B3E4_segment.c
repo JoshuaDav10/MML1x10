19d21b9dd45f6b35b89178e63ecd5253e49e7d94
Joshua David <joshua.david10x@gmail.com>
2025-08-15 01:10:07 -0500

    docs: Document splat symbol resolution issue
    Added a detailed log of the investigation into splat's symbol naming problem. The root cause is non-standard function prologues. The new strategy is to prioritize functions that splat can handle correctly, and I'm gonna isolate the non-standard ones to take care of later.

---
+++ src/rock_neo/func_8001B3E4_segment.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/func_8001B3E4_segment", D_8002ABE4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/func_8001B3E4_segment", func_8002AEE4);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/func_8001B3E4_segment", func_8002B2C8);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/func_8001B3E4_segment", func_8002B644);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/func_8001B3E4_segment", func_8002BD48);
+
+INCLUDE_ASM("config/../asm/rock_neo/nonmatchings/func_8001B3E4_segment", func_8002C0C4);
