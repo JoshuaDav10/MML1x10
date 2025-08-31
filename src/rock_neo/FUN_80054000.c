1520ff2184b43f26f259b06ff889d6c7279311c5
Joshua David <joshua.david10x@gmail.com>
2025-08-31 00:39:30 -0500

    Map 10 new functions: 80053F80-80054400 - total: 899 functions

---
+++ src/rock_neo/FUN_80054000.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_80054000", FUN_80054000);
---
+++ src/rock_neo/FUN_80054280.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_80054280", FUN_80054280);
