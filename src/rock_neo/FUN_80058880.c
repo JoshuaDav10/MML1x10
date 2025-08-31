ced718b26f62116e2c07520dff8d62ed40bbf927
Joshua David <joshua.david10x@gmail.com>
2025-08-31 17:33:47 -0500

    Map 6 more functions: 80058880-80058B00 - total: 1041 functions

---
+++ src/rock_neo/FUN_80058880.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_80058880", FUN_80058880);
---
+++ src/rock_neo/FUN_80058A80.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_80058A80", FUN_80058A80);
