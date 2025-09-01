f2610f773d52e00b06d3cbb2f618a0ce6db2ad6f
Joshua David <joshua.david10x@gmail.com>
2025-09-01 11:21:07 -0500

    Map 5 more functions in 0x6EXXX range with C stubs
    - FUN_8006d9f8 (0x6d9f8-0x6dc24): Large function with sp,-0x18
    - FUN_8006de00 (0x6de00-0x6ded0): Function with sp,-0x20
    - FUN_8006ded4 (0x6ded4-0x6dff0): Large function with sp,-0x18
    - FUN_8006dff4 (0x6dff4-0x6e3d8): Large function with sp,-0x20
    - FUN_8006e3dc (0x6e3dc-0x6e6f8): Large function with sp,-0x18
    
    Total functions mapped: 1169 (+5)
    All functions have corresponding C stub files created
    Progress: 10/20 functions mapped this session

---
+++ src/rock_neo/FUN_8006de00.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8006de00", FUN_8006de00);
---
+++ src/rock_neo/FUN_8006e3dc.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8006e3dc", FUN_8006e3dc);
