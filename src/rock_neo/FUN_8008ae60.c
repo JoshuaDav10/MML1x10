ea8b5a1d73f326d4096e679d39cddb832ff62a67
Joshua David <joshua.david10x@gmail.com>
2025-08-31 19:22:20 -0500

    Map 4 more functions in 0x8008XXXX range: FUN_8008ae60, FUN_8008ae90, FUN_8008aec0, FUN_8008aef0 - Total: 1051 functions

---
+++ src/rock_neo/FUN_8008ae60.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8008ae60", FUN_8008ae60);
---
+++ src/rock_neo/FUN_8008aec0.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8008aec0", FUN_8008aec0);
