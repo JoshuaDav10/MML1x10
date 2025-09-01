5824b66dc2d2cfd57cc9d497fb1f6f5e07c9e8e0
Joshua David <joshua.david10x@gmail.com>
2025-09-01 12:10:31 -0500

    Map 5 more functions in 0x7XXXX range with C stubs
    - FUN_800701d8 (0x701d8-0x70578): Large function with sp,-0x30
    - FUN_800706d8 (0x706d8-0x70858): Large function with sp,-0x18
    - FUN_8007085c (0x7085c-0x70880): Small function with sp,-0x18
    - FUN_80070884 (0x70884-0x708ac): Small function with sp,-0x18
    - FUN_800708b0 (0x708b0-0x709a8): Large function with sp,-0x40
    
    Total functions mapped: 1194 (+5)
    All functions have corresponding C stub files created
    Progress: 15/20 functions mapped this session

---
+++ src/rock_neo/FUN_800701d8.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_800701d8", FUN_800701d8);
---
+++ src/rock_neo/FUN_800708b0.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_800708b0", FUN_800708b0);
