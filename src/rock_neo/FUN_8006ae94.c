99e1c34c62116f00d48a3478acf6653513d226b4
Joshua David <joshua.david10x@gmail.com>
2025-09-01 01:03:05 -0500

    Add FUN_8006ae94, FUN_8006b0b0, and FUN_8006b0fc functions and C stubs (placement needs fixing)

---
+++ src/rock_neo/FUN_8006ae94.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8006ae94", FUN_8006ae94);
---
+++ src/rock_neo/FUN_8006b0fc.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8006b0fc", FUN_8006b0fc);
