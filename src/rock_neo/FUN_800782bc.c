cf251f83238370de31534201aa196c7d13b2afc3
Joshua David <joshua.david10x@gmail.com>
2025-09-02 10:26:56 -0500

    Map functions FUN_800782bc, FUN_800783e8, FUN_8007852c - Session 4 functions 1-3 (1300 total functions)

--- src/rock_neo/FUN_800782bc.c
+++ src/rock_neo/FUN_800782bc.c
@@ -1,2 +1,3 @@
 #include "common.h"
 INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_800782bc", FUN_800782bc);
+
--- src/rock_neo/FUN_800783e8.c
+++ src/rock_neo/FUN_800783e8.c
@@ -1,2 +1,3 @@
 #include "common.h"
 INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_800783e8", FUN_800783e8);
+
