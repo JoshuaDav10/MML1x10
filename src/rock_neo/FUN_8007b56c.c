67c8f34fa94ada6ea225e31b36d841f10067fb6a
Joshua David <joshua.david10x@gmail.com>
2025-09-05 19:28:38 -0500

    Map functions FUN_8007b56c to FUN_8007c6a0 - 9 functions mapped

---
+++ src/rock_neo/FUN_8007b56c.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM(s_rock_neo, "asm/rock_neo/nonmatchings/FUN_8007b56c/FUN_8007b56c.s");
---
+++ src/rock_neo/FUN_8007bd60.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM(s_rock_neo, "asm/rock_neo/nonmatchings/FUN_8007bd60/FUN_8007bd60.s");
