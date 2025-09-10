9509f8917e70a9b34b8cf78d30b64eb1c94fe162
Joshua David <joshua.david10x@gmail.com>
2025-09-09 22:40:56 -0500

    Map 2 new functions: FUN_80076620, FUN_8007665c - now at 1,356 total

---
+++ src/rock_neo/FUN_80076620.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_80076620", FUN_80076620);
---
+++ src/rock_neo/FUN_8007665c.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8007665c", FUN_8007665c);
