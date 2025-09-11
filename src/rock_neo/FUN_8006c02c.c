e4e205e5a0b7756f509ea450f5b93bbbb680e11d
Steam Deck User <deck@steamdeck.local>
2025-09-11 11:22:53 -0500

    Map 2 new functions in 0x8006XXXX range: FUN_8006c02c and FUN_8006c1a0 - now at 1,645 total functions

---
+++ src/rock_neo/FUN_8006c02c.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8006c02c", FUN_8006c02c);
---
+++ src/rock_neo/FUN_8006c1a0.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_8006c1a0", FUN_8006c1a0);
