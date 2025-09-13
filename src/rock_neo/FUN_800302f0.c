a83fddec7f2ba42d128226ed774da746678eb552
Steam Deck User <deck@steamdeck.local>
2025-09-13 00:04:06 -0500

    Map 3 more unmapped functions: FUN_800277d0, FUN_800302f0, FUN_80063a88
    - Successfully identified and mapped 3 additional unmapped functions
    - All functions mapped in correct address order in YAML configuration
    - Created corresponding C stub files for each mapped function
    - Function count increased from 1794 to 1797 (+3 functions)
    - Build verification successful
    - Total progress: 1797/2000 functions (89.85% complete)

---
+++ src/rock_neo/FUN_800302f0.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_800302f0", FUN_800302f0);
---
+++ src/rock_neo/FUN_80063a88.c
@@ -0,0 +1,2 @@
+#include "common.h"
+INCLUDE_ASM("asm/rock_neo/nonmatchings/FUN_80063a88", FUN_80063a88);
